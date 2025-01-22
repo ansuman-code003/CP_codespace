ef3357ea2eae90fbe3bdb5fa5db621e69e37becf
Ansuman Mazumder <112932950+ansuman-code003@users.noreply.github.com>
2025-01-22 17:07:56 +0530

    Create 896 Monotonic Array.cpp

---
+++ LeetCode/896 Monotonic Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int sz=nums.size();
+        bool type=false; // true = inc; false = dec
+        if(nums[0] > nums[sz-1]) type=true;
+        for(int i=0;i<sz-1;i++){
+            
+        }
+
+    }
+};
